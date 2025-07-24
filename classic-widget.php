f66cbe0a94097a3504b056cf5f52e0c244d22bde
hasankarim18 <hallysust@gmail.com>
2025-07-24 20:59:20 +0600

    first commit

---
+++ classic-widget.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Classic Widget
+Description: Restores the classic WordPress widgets interface.
+Version: 1.0.0
+Author: Your Name
+License: GPL2
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+define('CLASSIC_WIDGET_DIR', plugin_dir_path(__FILE__));
+define('CLASSIC_WIDGET_URL', plugin_dir_url(__FILE__));
+
